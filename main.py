cb21d4de9b20c995fd034eecb374f613fa226a90
ArthurA14 <77979858+ArthurA14@users.noreply.github.com>
2023-02-27 19:50:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import uvicorn
 from starlette.responses import JSONResponse, Response
 from starlette.middleware import Middleware
 from starlette_exporter import PrometheusMiddleware, handle_metrics
-from prometheus_client import Counter, Histogram, Info, Summary, generate_latest
+from prometheus_client import Counter, Histogram, Info, Summary, generate_latest, make_wsgi_app
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 import prometheus_client
 import joblib
 import schemas
@@ -18,15 +19,21 @@ api_router = FastAPI() # APIRouter()
 api_router.add_middleware(PrometheusMiddleware)
 api_router.add_route("/metrics", handle_metrics)
 
+# Add Prometheus middleware to the FastAPI application
+middleware = [Middleware(PrometheusMiddleware)]
+api_router = FastAPI(middleware=middleware)
+
 with open('rf.sav', 'rb') as file :
     loaded_model = joblib.load(file)
 
 # Define the metric variables
-predict_calls_counter = Counter('total_predict_calls', 'Number of predict requests to the API endpoint')
+predict_calls_counter = Counter('total_predict_calls', 'Number of predict requests to the API endpoint', ['method', 'endpoint'])
 predict_call_duration = Histogram('predict_latency_seconds', 'Latency time of the API for a predict call', ['method']) # ValueError: No label names were set when constructing histogram:predict_latency_seconds
-api_call_info = Info('api_info', 'Information about the API', ["method", "first_endpoint"])
-api_call_summary = Summary('api_call_bytes_size', 'Size of the API call', ["method", "second_endpoint"])
+api_call_info = Info('api_info', 'Information about the API', ["method", "second_endpoint"])
+api_call_summary = Summary('api_call_bytes_size', 'Size of the API call', ["method", "third_endpoint"])
 
+
+############################################################ PREDICT #################################################################
 
 @api_router.post("/predict", status_code=200)
 async def predict(input_data: schemas.Iris) :
@@ -57,7 +64,7 @@ async def predict(input_data: schemas.Iris) :
     labeled_pred = {'prediction': iris_pred, 'score': pred[0]}
 
     # Increment the counter
-    predict_calls_counter.inc()
+    predict_calls_counter.labels('POST').inc()
 
     # Get the request time duration
     predict_call_duration.labels(pred).observe(time.time() - start_time)
@@ -67,21 +74,29 @@ async def predict(input_data: schemas.Iris) :
     # curl 'http://localhost:8080/predict' -H 'Content-Type: application/json' -d '{"sepal_l": 5, "sepal_w": 2, "petal_l": 3, "petal_w": 4}'
 
 
+############################################################ METRICS #################################################################
+
 # Define the metrics endpoint
-@api_router.post("/metrics") # @api_router.get("/metrics")
-def get_counter() :
-    return generate_latest(predict_calls_counter).decode("utf-8") # return generate_latest().decode("utf-8")
-    # curl 'http://localhost:8080/metrics' -H 'Content-Type: application/json' -d '{"sepal_l": 5, "sepal_w": 2, "petal_l": 3, "petal_w": 4}' | tr -d '\n' | tr '#' '\n' | grep -v '^$'
+@api_router.get("/metrics")
+async def metrics() :
+    return Response(content=generate_latest(), media_type='text/plain') # return generate_latest().decode("utf-8")
+    # curl 'http://localhost:8080/metrics' -H 'Content-Type: application/json'| tr -d '\n' | tr '#' '\n' | grep -v '^$'
+
+
+@api_router.post("/metrics/calls_count")
+async def get_counter() :
+    return generate_latest(predict_calls_counter).decode("utf-8")
+    # curl 'http://localhost:8080/metrics/calls_count' -H 'Content-Type: application/json' -d '{"sepal_l": 5, "sepal_w": 2, "petal_l": 3, "petal_w": 4}' | tr -d '\n' | tr '#' '\n' | grep -v '^$'
 
 
 @api_router.post('/metrics/duration')
-def get_duration() :
+async def get_duration() :
     return generate_latest(predict_call_duration).decode("utf-8")
     # curl 'http://localhost:8080/metrics/duration' -H 'Content-Type: application/json' -d '{"sepal_l": 5, "sepal_w": 2, "petal_l": 3, "petal_w": 4}' | tr -d '\n' | tr '#' '\n' | grep -v '^$'
 
 
 @api_router.post("/api/call/info")
-def api_call_info_post() :
+async def api_call_info_post() :
     endpoint = "/api/call/info"
     api_call_info.labels("POST", "info").info({"endpoint": endpoint, "info": api_call_info.info})
     return {"status": "success"}
@@ -89,12 +104,13 @@ def api_call_info_post() :
 
 
 @api_router.post("/api/call/summary")
-def api_call_summary_post() :
+async def api_call_summary_post() :
     start = time.time()
     # Perform API processing here
     api_call_summary.labels("POST", "elapsed_time").observe(time.time() - start)
     return {"status": "success"}
     # curl 'http://localhost:8080/api/call/summary' -H 'Content-Type: application/json' -d '{"sepal_l": 5, "sepal_w": 2, "petal_l": 3, "petal_w": 4}' | tr -d '\n' | tr '#' '\n' | grep -v '^$'
+
 
 
 if __name__ == '__main__' :
